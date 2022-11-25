4fb252924bd94dbd02392389c6c8d1de00d65957
Antoine CAVALLERA <antoine.cavallera@gmail.com>
2022-11-26 00:55:18 +0100

    add player and particles

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ var Engine = Matter.Engine,
     Runner = Matter.Runner,
     MouseConstraint = Matter.MouseConstraint,
     Mouse = Matter.Mouse,
+    Constraint = Matter.Constraint,
+    Composites = Matter.Composites,
     Composite = Matter.Composite,
-    Bodies = Matter.Bodies;
+    Common = Matter.Common,
+    Bodies = Matter.Bodies,
+    Bounds = Matter.Bounds;
+    
 
 // create engine
 var engine = Engine.create(),
@@ -21,9 +26,15 @@ var render = Render.create({
     options: {
         width: 800,
         height: 600,
-        showVelocity: true
+        showVelocity: false,
+
+        wireframes: false,
+        background: '#606060',
+        hasBounds: true,
     }
 });
+
+var bullet = [];
 
 Render.run(render);
 
@@ -31,19 +42,29 @@ Render.run(render);
 var runner = Runner.create();
 Runner.run(runner, engine);
 
-// add bodies
-Composite.add(world, [
-    // falling blocks
-    Bodies.rectangle(200, 100, 60, 60, { frictionAir: 0.00001 }),
-    Bodies.rectangle(400, 100, 60, 60, { frictionAir: 0.05 }),
-    Bodies.rectangle(600, 100, 60, 60, { frictionAir: 0.1 }),
+// player
+var player = Bodies.circle(200, 100, 40, 
+    { label: 'player', frictionAir: 0.6, fillStyle: 'red', strokeStyle: 'blue',lineWidth: 3});
+Composite.add(world, player);
 
-    // walls
-    Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
-    Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
-    Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
-    Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
-]);
+// particles stack
+var particleOptions = { 
+    friction: 0.05,
+    frictionStatic: 0.1,
+    render: { visible: true } 
+};
+var particles = softBody(250, 100, 5, 5, 0, 0, true, 18, particleOptions);
+Composite.add(world, particles);
+
+var wall = Bodies.rectangle(50, 0, 800, 50, { isStatic: false });
+var constraint = Constraint.create({
+    pointA: { x: -100, y: -100 },
+    bodyB: wall,
+    pointB: { x: -10, y: -10 },
+    stiffness: 0.001,
+    damping: 0.1
+});
+Composite.add(world, [wall, constraint]);
 
 // add mouse control
 var mouse = Mouse.create(render.canvas),
@@ -57,13 +78,92 @@ var mouse = Mouse.create(render.canvas),
         }
     });
 
+// keyboard control
+const keyHandlers = {
+
+    KeyD: () => {
+        Matter.Body.applyForce(player, {
+        x: player.position.x,
+        y: player.position.y
+        }, {x: 0.02, y: 0})
+    },
+    KeyA: () => {
+        Matter.Body.applyForce(player, {
+        x: player.position.x,
+        y: player.position.y
+        }, {x: -0.02, y: 0})
+    },
+    KeyS: () => {
+        Matter.Body.applyForce(player, {
+        x: player.position.x,
+        y: player.position.y
+        }, {x: 0, y: 0.02})
+    },
+    KeyW: () => {
+        Matter.Body.applyForce(player, {
+        x: player.position.x,
+        y: player.position.y
+        }, {x: 0, y: -0.02})
+    },
+    Space: () => {
+        bullet.push(Bodies.circle(200, 100, 2, 
+            { label: 'bullet', frictionAir: 0.1, strokeStyle: 'red'}));
+
+        var lastBullet = bullet.pop();
+        Composite.add(world, lastBullet);
+
+        Matter.Body.set(lastBullet, "position", {x: player.position.x, y: player.position.y})
+        Matter.Body.applyForce(lastBullet, {
+            x: lastBullet.position.x,
+            y: lastBullet.position.y
+            }, {x: (mouse.position.x - lastBullet.position.x) * 0.00001, y: (mouse.position.y - lastBullet.position.y) * 0.00001})
+
+        // const particlesBodies = Composite.allBodies(stack);
+
+        // for (var i = 0; i < particlesBodies.length; i++) {
+        //     var body = particlesBodies[i];
+
+        //     Matter.Body.applyForce(body, {
+        //         x: body.position.x,
+        //         y: body.position.y
+        //         }, {x: Common.random(-0.001, 0.001), y: Common.random(-0.001, 0.001)})
+        //     }
+    },
+};
+    
+const keysDown = new Set();
+document.addEventListener("keydown", event => {
+    keysDown.add(event.code);
+});
+document.addEventListener("keyup", event => {
+    keysDown.delete(event.code);
+});
+
+Matter.Events.on(engine, "beforeUpdate", event => {
+    [...keysDown].forEach(k => {
+        keyHandlers[k]?.();
+    });
+    Render.lookAt(render, player, {
+        x: 400,
+        y: 300
+      })
+});
+
 Composite.add(world, mouseConstraint);
 
 // keep the mouse in sync with rendering
 render.mouse = mouse;
 
-// fit the render viewport to the scene
-Render.lookAt(render, {
-    min: { x: 0, y: 0 },
-    max: { x: 800, y: 600 }
-});
+// Utils
+function softBody(xx, yy, columns, rows, columnGap, rowGap, crossBrace, particleRadius, particleOptions, constraintOptions) {
+    particleOptions = Common.extend({ inertia: Infinity }, particleOptions);
+    constraintOptions = Common.extend({ stiffness: 0.8, render: { type: 'line', anchors: false } }, constraintOptions);
+
+    var softBody = Composites.stack(xx, yy, columns, rows, columnGap, rowGap, function(x, y) {
+        return Bodies.circle(x, y, particleRadius, particleOptions);
+    });
+
+    softBody.label = 'Soft Body';
+
+    return softBody;
+};

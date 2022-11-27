770814f450061ba5fba105d992ab7e62e0f9ab16
Antoine CAVALLERA <antoine.cavallera@gmail.com>
2022-11-27 19:10:56 +0100

    custom render an improve player

--- app.js
+++ app.js
@@ -37,40 +37,68 @@ var render = Render.create({
     }
 });
 
+var floorMap = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        ]
+
 var map = [
     [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
-    [3,1,1,1,1,1,5,1,3,1,1,1,1,1,5],
-    [3,1,1,1,1,1,5,1,3,1,1,1,1,1,5],
-    [3,1,1,1,1,1,7,1,8,1,1,1,1,1,5],
-    [3,1,1,1,1,1,5,1,3,1,1,1,1,1,5],
-    [3,1,1,1,1,1,5,1,3,1,1,1,1,1,5],
-    [3,4,4,4,6,4,5,1,3,1,1,1,1,1,5],
-    [3,1,1,1,1,1,7,1,3,1,1,1,1,1,5],
-    [3,1,1,1,1,1,5,1,3,1,1,1,1,1,5],
-    [3,1,1,1,1,1,5,1,3,4,4,6,4,4,5],
-    [3,1,1,1,1,1,5,1,3,1,1,1,1,1,5],
-    [3,1,1,1,1,1,5,1,3,1,1,1,1,1,5],
-    [3,1,1,1,1,1,7,1,8,1,1,1,1,1,5],
-    [3,1,1,1,1,1,5,1,3,1,1,1,1,1,5],
+    [3,0,0,0,0,0,5,0,3,0,0,0,0,0,5],
+    [3,0,0,0,0,0,5,0,3,0,0,0,0,0,5],
+    [3,0,0,0,0,0,7,0,8,0,0,0,0,0,5],
+    [3,0,0,0,0,0,5,0,3,0,0,0,0,0,5],
+    [3,0,0,0,0,0,5,0,3,0,0,0,0,0,5],
+    [3,4,9,9,9,4,5,0,3,0,0,0,0,0,5],
+    [3,0,0,0,0,0,7,0,3,0,0,0,0,0,5],
+    [3,0,0,0,0,0,5,0,3,0,0,0,0,0,5],
+    [3,0,0,0,0,0,5,0,3,4,4,6,4,4,5],
+    [3,0,0,0,0,0,5,0,3,0,0,0,0,0,5],
+    [3,0,0,0,0,0,5,0,3,0,0,0,0,0,5],
+    [3,0,0,0,0,0,7,0,8,0,0,0,0,0,5],
+    [3,0,0,0,0,0,5,0,3,0,0,0,0,0,5],
     [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4]
         ]
 		   
-var elemsToDraw = [];
-
-mapInit();
-
+initFloorMap();
+initMap();
 
 var boxes = Composites.stack(1000, 50, 4, 4, 5, 5, function(x, y) {
     return Bodies.rectangle(x, y, 64, 64, { isStatic: false ,density: 0.8, frictionAir: 0.8, render: { sprite: { texture: './assets/img/box.png'}}});
 });
 Composite.add(engine.world, boxes);
 
+
 var player = Bodies.rectangle(500, 500, 51, 29,  {collisionFilter: {  category: 1 }, density: 0.05, frictionAir: 0.8,  isStatic: false , render: { sprite: { texture: './assets/img/patrick.png'}}});
 Composite.add(engine.world, player);
 var playerLookAt = 0;
 
-console.log(player.mass)
-
+var ammo = Bodies.rectangle(-100,-100, 1, 1, {
+    isStatic : true,
+	render:{
+		fillStyle:"#C44D58",
+		text:{
+			content:"300 / 300",
+			color:"white",
+			size:100,
+			family:"Papyrus",
+		},
+	},
+});
+Composite.add(engine.world, ammo);
 
 var bullet = [];
 
@@ -139,11 +167,24 @@ const keyHandlers = {
         }
     },
     Space: () => {
-        bullet.push(Bodies.circle(200, 100, 2, 
-            { label: 'bullet', frictionAir: 0.1, strokeStyle: 'red'}));
+        bullet.push(Bodies.circle(200, 100, 3, 
+            { label: 'bullet', frictionAir: 0.001, dentisy: 0.1, 
+             render: {
+                fillStyle: 'black',
+                strokeStyle: 'black',
+                lineWidth: 1
+           }}));
 
-        var lastBullet = bullet.pop();
+        var lastBullet = bullet[bullet.length - 1];
+        var firsBullet = bullet[0];
+
+        console.log(lastBullet.mass)
         Composite.add(world, lastBullet);
+
+        if (bullet.length > 500) {
+            Composite.remove(world, firsBullet);
+            bullet.shift();
+        }
 
         var angle = getAnglePlayerMouse(player.position.x, player.position.y, mouse.position.x, mouse.position.y);
 
@@ -151,7 +192,7 @@ const keyHandlers = {
         Matter.Body.applyForce(lastBullet, {
             x: lastBullet.position.x,
             y: lastBullet.position.y
-            }, {x: (mouse.position.x - lastBullet.position.x) * 0.000001, y: (mouse.position.y - lastBullet.position.y) * 0.000001})
+            }, {x: Math.cos(playerLookAt - Math.PI/2) * 0.002, y: Math. sin(playerLookAt - Math.PI/2) * 0.002})
     },
 };
 
@@ -180,90 +221,200 @@ Matter.Events.on(engine, "beforeUpdate", event => {
         keyHandlers[k]?.();
     });
     Render.lookAt(render, player, {
-        x: 800,
-        y: 600
+        x: windowWidht,
+        y: windowHeight
       })
-      console.log(playerLookAt);
+
+    // var lastBullet = bullet[bullet.length - 1];
+    // if (Matter.Collision.collides(player, lastBullet) != null) {
+    //     console.log("collision");
+    // }
 });
 
-function mapInit()
+Matter.Events.on(engine, 'glassCollision', function(event) {
+    var pairs = event.pairs;
+
+    console.log(pairs)
+
+
+});
+
+function initFloorMap()
 {
-	let k = 0;
-	for(let i = 0; i < map.length; i++) {
-		for(let j = 0; j < map[i].length; j++) {
-			if(map[j][i] == 1)	
-			elemsToDraw[k] =  Bodies.rectangle(i*100, j*100, 100, 100,  {  collisionFilter: { category: 0}, isStatic: true , render: { sprite: { texture: './assets/img/ground.png'}}});
-			else if(map[j][i] == 2)
-				elemsToDraw[k] = Bodies.rectangle(i*100, j*100 + 35, 100, 30, { isStatic: true ,render: { sprite: { texture: './assets/img/wall_hori.png'}}});
-			else if(map[j][i] == 3)
-				elemsToDraw[k] = Bodies.rectangle(i*100 + 35, j*100, 30, 100, { isStatic: true ,render: { sprite: { texture: './assets/img/wall_vert.png'}}});
-			else if(map[j][i] == 4)
-				elemsToDraw[k] = Bodies.rectangle(i*100, j*100 - 35, 100, 30, { isStatic: true ,render: { sprite: { texture: './assets/img/wall_hori.png'}}});
-			else if(map[j][i] == 5)
-				elemsToDraw[k] = Bodies.rectangle(i*100 - 35, j*100, 30, 100, { isStatic: true ,render: { sprite: { texture: './assets/img/wall_vert.png'}}});
+    var elemsToDraw = [];
+    var k = 0;
+	for(let i = 0; i < floorMap.length; i++) {
+		for(let j = 0; j < floorMap[i].length; j++) {
+            var type = floorMap[j][i];
 
-            // Bottom Left Doors
-            else if(map[j][i] == 6) {
-                elemsToDraw[k] = Bodies.rectangle(i*100, j*100 - 35, 30, 100, { isStatic: false , frictionAir: 0.001, render: { sprite: { texture: './assets/img/door_hori.png'}}});
-                var pivot = Constraint.create({
-                    pointA: {x : elemsToDraw[k].position.x - 50, y: elemsToDraw[k].position.y},
-                    bodyB: elemsToDraw[k],
-                    pointB: {x : -50, y: 0},
-                    stiffness: 1,
-                    damping: 0.1
-                });
-                var groom = Constraint.create({
-                    pointA: {x : elemsToDraw[k].position.x -50, y: elemsToDraw[k].position.y - 15},
-                    bodyB: elemsToDraw[k],
-                    pointB: {x : -50, y: -15},
-                    stiffness: 0.005,
-                    damping: 0.1
-                });
-                Composite.add(engine.world, [pivot, groom]);
-            }
+            switch(type) {
+                case 1:
+                    elemsToDraw[k] = 
+                    Bodies.rectangle(i*100, j*100, 100, 100,  
+                    { collisionFilter: { category: 0}, 
+                        isStatic: true, 
+                        render: { sprite: { texture: './assets/img/ground.png'}}
+                    });
+                    k++;
+                    break;
 
-            // Ver Doors Left
-            else if(map[j][i] == 7) {
-                elemsToDraw[k] = Bodies.rectangle(i*100 - 35, j*100, 30, 95, { isStatic: false , frictionAir: 0.001, render: { sprite: { texture: './assets/img/door_vert.png'}}});
-                var pivot = Constraint.create({
-                    pointA: {x : elemsToDraw[k].position.x, y: elemsToDraw[k].position.y-50},
-                    bodyB: elemsToDraw[k],
-                    pointB: {x : 0, y: -50},
-                    stiffness: 0.05,
-                    damping: 0.1
-                });
-                var groom = Constraint.create({
-                    pointA: {x : elemsToDraw[k].position.x + 15, y: elemsToDraw[k].position.y -50},
-                    bodyB: elemsToDraw[k],
-                    pointB: {x : +15, y: -50},
-                    stiffness: 0.0003,
-                    damping: 0.1,
-                });
-                Composite.add(engine.world, [pivot, groom]);
+                default:
+                    console.log("Element id: %d unknow", type);
+                    break;
             }
-
-            // Ver Doors Right
-            else if(map[j][i] == 8) {
-                elemsToDraw[k] = Bodies.rectangle(i*100 + 35, j*100, 30, 95, { isStatic: false , frictionAir: 0.001, render: { sprite: { texture: './assets/img/door_vert.png'}}});
-                var pivot = Constraint.create({
-                    pointA: {x : elemsToDraw[k].position.x, y: elemsToDraw[k].position.y-50},
-                    bodyB: elemsToDraw[k],
-                    pointB: {x : 0, y: -50},
-                    stiffness: 0.05,
-                    damping: 0.1
-                });
-                var groom = Constraint.create({
-                    pointA: {x : elemsToDraw[k].position.x + 15, y: elemsToDraw[k].position.y -50},
-                    bodyB: elemsToDraw[k],
-                    pointB: {x : +15, y: -50},
-                    stiffness: 0.0003,
-                    damping: 0.1,
-                });
-                Composite.add(engine.world, [pivot, groom]);
-            }
-			k++;
 		}
 	}
+    Composite.add(engine.world, elemsToDraw);
+}
+
+function initMap()
+{
+    var elemsToDraw = [];
+	var k = 0;
+	for(let i = 0; i < map.length; i++) {
+		for(let j = 0; j < map[i].length; j++) {
+            var type = map[j][i];
+
+            switch(type) {
+                case 2:
+                    elemsToDraw[k] = 
+                    Bodies.rectangle(i*100, j*100 + 35, 100, 30, 
+                        { isStatic: true , 
+                        render: { sprite: { texture: './assets/img/wall_hori.png'}}
+                    });
+                    k++;
+                    break;
+
+                case 3:
+                    elemsToDraw[k] = 
+                    Bodies.rectangle(i*100 + 35, j*100, 30, 100, 
+                        { isStatic: true ,
+                        render: { sprite: { texture: './assets/img/wall_vert.png'}}
+                    });
+                    k++;
+                    break;
+
+                case 4:
+                    elemsToDraw[k] = 
+                    Bodies.rectangle(i*100, j*100 - 35, 100, 30, 
+                        { isStatic: true ,
+                        render: { sprite: { texture: './assets/img/wall_hori.png'}}
+                    });
+                    k++;
+                    break;
+
+                case 5:
+                    elemsToDraw[k] = 
+                    Bodies.rectangle(i*100 - 35, j*100, 30, 100, 
+                        { isStatic: true ,
+                        render: { sprite: { texture: './assets/img/wall_vert.png'}}
+                    });
+                    k++;
+                    break;
+
+                // Door Hori
+                case 6:
+                    elemsToDraw[k] = 
+                    Bodies.rectangle(i*100, j*100 - 35, 30, 100, 
+                        { isStatic: false , 
+                        frictionAir: 0.001, 
+                        render: { sprite: { texture: './assets/img/door_hori.png'}}
+                    });
+                    var pivot = 
+                    Constraint.create({
+                        pointA: {x : elemsToDraw[k].position.x - 50, y: elemsToDraw[k].position.y},
+                        bodyB: elemsToDraw[k],
+                        pointB: {x : -50, y: 0},
+                        stiffness: 1,
+                        damping: 0.1
+                    });
+                    var groom =
+                     Constraint.create({
+                        pointA: {x : elemsToDraw[k].position.x -50, y: elemsToDraw[k].position.y - 15},
+                        bodyB: elemsToDraw[k],
+                        pointB: {x : -50, y: -15},
+                        stiffness: 0.005,
+                        damping: 0.1
+                    });
+                    Composite.add(engine.world, [pivot, groom]);
+                    k++;
+                    break;
+
+                // Door Verti Left
+                case 7:
+                    elemsToDraw[k] = 
+                    Bodies.rectangle(i*100 - 35, j*100, 30, 95, 
+                        { isStatic: false , 
+                        frictionAir: 0.001, 
+                        render: { sprite: { texture: './assets/img/door_vert.png'}}
+                    });
+                    var pivot = Constraint.create({
+                        pointA: {x : elemsToDraw[k].position.x, y: elemsToDraw[k].position.y-50},
+                        bodyB: elemsToDraw[k],
+                        pointB: {x : 0, y: -50},
+                        stiffness: 0.05,
+                        damping: 0.1
+                    });
+                    var groom = Constraint.create({
+                        pointA: {x : elemsToDraw[k].position.x + 15, y: elemsToDraw[k].position.y -50},
+                        bodyB: elemsToDraw[k],
+                        pointB: {x : +15, y: -50},
+                        stiffness: 0.0003,
+                        damping: 0.1,
+                    });
+                    Composite.add(engine.world, [pivot, groom]);
+                    k++;
+                    break;
+
+                // Door Verti Right
+                case 8:
+                    elemsToDraw[k] = 
+                    Bodies.rectangle(i*100 + 35, j*100, 30, 95, 
+                        { isStatic: false , 
+                        frictionAir: 0.001,
+                         render: { sprite: { texture: './assets/img/door_vert.png'}}
+                    });
+                    var pivot = Constraint.create({
+                        pointA: {x : elemsToDraw[k].position.x, y: elemsToDraw[k].position.y-50},
+                        bodyB: elemsToDraw[k],
+                        pointB: {x : 0, y: -50},
+                        stiffness: 0.05,
+                        damping: 0.1
+                    });
+                    var groom = Constraint.create({
+                        pointA: {x : elemsToDraw[k].position.x + 15, y: elemsToDraw[k].position.y -50},
+                        bodyB: elemsToDraw[k],
+                        pointB: {x : +15, y: -50},
+                        stiffness: 0.0003,
+                        damping: 0.1,
+                    });
+                    Composite.add(engine.world, [pivot, groom]);
+                    k++;
+                    break;
+
+                // Window
+                case 9:
+                    elemsToDraw[k] = Composites.stack(i*100 - 50, j*100 - 40, 20, 2, 0, 0, function(x, y) {
+                        return Bodies.rectangle(x, y, 5, 5, {
+                            isSleeping : true,
+                            frictionAir : 0.01,
+                            render: {
+                                fillStyle: 'white',
+                                strokeStyle: 'black',
+                                lineWidth: 2
+                           }
+                        });
+                    });
+                    k++;
+                    break;
+
+                default:
+                    console.log("Element id: %d unknow", type);
+                    break;
+                    
+            }
+        }
+    }
     Composite.add(engine.world, elemsToDraw);
 }
 
--- libraries/matter.js
+++ libraries/matter.js
@@ -6824,6 +6824,33 @@ var Mouse = __webpack_require__(13);
                 }
 
                 c.globalAlpha = 1;
+
+                if(part.render.text)
+                {
+                    //30px is default font size
+                    var fontsize = 30;
+                    //arial is default font family
+                    var fontfamily = part.render.text.family || "Arial"; 
+                    //white text color by default
+                    var color = part.render.text.color || "#FFFFFF";
+
+                    if(part.render.text.size)
+                        fontsize = part.render.text.size;
+                    else if(part.circleRadius)
+                        fontsize = part.circleRadius/2;
+
+                    var content = "";
+                    if(typeof part.render.text == "string")
+                        content = part.render.text;
+                    else if(part.render.text.content)
+                        content = part.render.text.content;
+
+                    c.textBaseline="middle";
+                    c.textAlign="center";
+                    c.fillStyle=color;
+                    c.font = fontsize+'px '+fontfamily;
+                    c.fillText(content,part.position.x,part.position.y);
+                }
             }
         }
     };
